d3144fdcb6de00651fc6e6b70b0034f4a4f00be3
uyouid <yusuitoui@gmail.com>
2026-02-19 23:18:56 +0900

    add test

--- test/test_calc.cpp
+++ test/test_calc.cpp
@@ -11,3 +11,4 @@ TEST_GROUP(CalcGroup){
 TEST(CalcGroup, AddTest) { CHECK_EQUAL(5, add(2, 3)); }
 
 TEST(CalcGroup, MinusTest) { CHECK_EQUAL(-1, minus(2, 3)); }
+TEST(CalcGroup, MinusTest) { CHECK_EQUAL(2, minus(5, 3)); }
